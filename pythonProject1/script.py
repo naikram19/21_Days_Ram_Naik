b9a73c1d441ab53d573f90ff3eb8dabe129801e7
Ram Naik <naikram1986@gmail.com>
2021-07-12 13:00:25 +0530

    python variables, datatypes and typecasting

--- pythonProject1/script.py
+++ pythonProject1/script.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+#print("Hello World!")
+#x = True
+#print(x)
+#print(type(x))
+
+#y = 9
+#z = type(int(y))
+#print(z)
+
+# x = 4
+# print(type(x))
+# print(complex(x))
+# print(type(complex(x)))
+
+# x = input("Enter a number:")
+# print(x)
+# print(type(int(x)))
+
